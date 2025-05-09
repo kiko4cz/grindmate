54c0a33768bbe3cf132bf937051fad194af27a7c
kiko4cz <wizardeu@seznam.cz>
2025-05-09 06:13:04 +0200

    Fix: vyřešené konflikty a opravené soubory

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -7,16 +7,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
->>>>>>> b846b6a64f92c6b224d79f2e26e79639b2294239
+  </StrictMode>
 )
