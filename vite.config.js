6eb6c71a02d46055b1d42287362694b7d3b91b50
kiko4cz <wizardeu@seznam.cz>
2025-05-09 06:09:55 +0200

    Fix: resolve merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-=======
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -13,5 +7,5 @@ export default defineConfig({
   plugins: [react()
     , tailwindcss()
   ],
->>>>>>> b846b6a64f92c6b224d79f2e26e79639b2294239
+
 })
